58188c31736bae9145d0a0c9f8c6ce68a4f7c8a8
bocchi-chan <ihavenocluelolll@outlook.com>
2026-03-23 16:44:19 +0200

    stack

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+pub mod vm;
+
+use vm::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut s = stack::Stack::new(1);
+
+    if let Err(_) = s.push(2) {
+        println!("Failed to add 2");
+    }
+
+    if let Err(_) = s.push(3) {
+        println!("Failed to add 3");
+    }
+
+    match s.pop() {
+        Ok(v) => println!("{}", v),
+        Err(err) => println!("{:?}", err),
+    };
 }
